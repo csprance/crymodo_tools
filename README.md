210e5b36d20c4bb4248e5eb9d4554741f9b1a574
Chris Sprance <chrissprance@gmail.com>
2019-10-09 02:01:03 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Modo CryExport
+> CRYENGINE exporter for modo 13^
+
+
+## Customer User Channels
+### proxy_type
+> What type of proxy material
+
+    * NODRAW
+    * NOCOLLIDE
+    * OBSTRUCT
+    * DEFAULT
+    * NONE
+
+### udp_type
+> Defines the type of primitive proxy if it's not defined it's considered a RENDERMESH
+    
+    * RENDERMESH
+    * BOX
+    * CYLINDER
+    * SPHERE
+
+### master_material
+> The name of the master material that will be assigned to the object
+
+### mat_order
+> The order in which each sub-material should be placed.
+    
+---
+
+![](https://csprance.com/shots/2019-10-03_29981419-5cd2-4938-8da9-1fcdf4fb165a.png)
+
+![](https://csprance.com/shots/2019-10-03_ff922040-9fdf-4a24-b77f-4c5e7db65d5d.png)
---
+++ lxserv/commander_breakfast.py
@@ -0,0 +1,37 @@
+import lx
+import modo
+
+import cs_cry_export
+
+"""A simple example of a blessed MODO command using the commander module.
+https://github.com/adamohern/commander for details"""
+
+
+class CommandClass(cs_cry_export.CommanderClass):
+
+    def commander_arguments(self):
+        return [
+            {
+                "name": "dish1",
+                "datatype": "string",
+                "default": "bacon",
+                "values_list_type": "popup",
+                "values_list": ["bacon", "quinoa"],
+            },
+            {
+                "name": "dish2",
+                "datatype": "string",
+                "default": "eggs",
+                "values_list_type": "sPresetText",
+                "values_list": ["eggs", "kale"],
+            },
+        ]
+
+    def commander_execute(self, msg, flags):
+        dish1 = self.commander_arg_value(0)
+        dish2 = self.commander_arg_value(1)
+
+        modo.dialogs.alert("breakfast", " and ".join([dish1, dish2]))
+
+
+lx.bless(CommandClass, "cs_cry_export.breakfast")
